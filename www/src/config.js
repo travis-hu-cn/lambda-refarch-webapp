db0cb066e098ac6c290dd0a3bcd46ee6ffcf8170
travis.hu <travis.hu@emc.com>
2020-07-30 12:24:24 +0800

    update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "coginto_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "198rdm7ei42hp5cb0kp42jr7bu",     // CognitoClientID
+  "api_base_url": "https://iemz1hpclf.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "coginto_hosted_domain": "mytodoappdemo-travis-todo2.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d12xup3869m8nr.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
